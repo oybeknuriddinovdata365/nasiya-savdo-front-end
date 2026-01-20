6caa393b0bd4f5f393b69fc43f5e618392f3e2bb
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-01-20 11:31:18 +0500

    fixed

--- src/components/common/SkeletonRow.tsx
+++ src/components/common/SkeletonRow.tsx
@@ -1,20 +1,20 @@
 export function SkeletonRow() {
   return (
-    <tr className="animate-puls flex justify-between">
+    <tr className="animate-pulse flex justify-between">
       <td className="p-4">
-        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded"></div>
+        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded animate-pulse"></div>
       </td>
       <td className="p-4">
-        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded"></div>
+        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded animate-pulse"></div>
       </td>
       <td className="p-4">
-        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded"></div>
+        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded animate-pulse"></div>
       </td>
       <td className="p-4">
-        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded"></div>
+        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded animate-pulse"></div>
       </td>
       <td className="p-4">
-        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded"></div>
+        <div className="h-7 w-40 bg-gray-200 dark:bg-gray-600 rounded animate-pulse"></div>
       </td>
     </tr>
   );
